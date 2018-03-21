2f684bf668026f3871541a2a607ed1aec03fd67d
xiaopandjsj <36355307+xiaopandjsj@users.noreply.github.com>
2018-03-22 00:41:06 +0800

    Add files via upload

---
+++ js/countdown-js.js
@@ -0,0 +1,30 @@
+// set the date we're counting down to
+var target_date = new Date('Jun, 30, 1995').getTime();
+ 
+// variables for time units
+var days, hours, minutes, seconds;
+ 
+// get tag element
+var countdown = document.getElementById('countdown');
+ 
+// update the tag with id "countdown" every 1 second
+setInterval(function () {
+ 
+    // find the amount of "seconds" between now and target
+    var current_date = new Date().getTime();
+    var seconds_left = (current_date - target_date) / 1000;
+ 
+    // do some time calculations
+    days = parseInt(seconds_left / 86400);
+    seconds_left = seconds_left % 86400;
+     
+    hours = parseInt(seconds_left / 3600);
+    seconds_left = seconds_left % 3600;
+     
+    minutes = parseInt(seconds_left / 60);
+    seconds = parseInt(seconds_left % 60);
+     
+    // format countdown string + set tag value
+    countdown.innerHTML = '<div class="days"><h2>Live <span>' + days +  ' </span>Days</div> ';  
+ 
+}, 1000);
